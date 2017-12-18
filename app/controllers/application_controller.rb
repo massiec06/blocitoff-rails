0a45122bb49b586bda4d7f6bef16c601c621d6fb
Chris Massie <massiec06@gmail.com>
2017-12-18 10:12:02 -0500

    checkpoint 4 profile completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  protected
+    def after_sign_in_path_for(resource)
+      if current_user.present?
+        stored_location_for(resource) || user_path(current_user) || root_path
+      else
+        super
+      end
+    end
 end
